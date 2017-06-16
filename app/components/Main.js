15d64c8b631d3c029a5465a1563446860e20988c
radhika sivarajan <radhikaraj333@gmail.com>
2017-06-16 11:17:42 -0700

    Navbar and footer component.

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,27 +1,15 @@
 var React = require("react");
 var Link = require("react-router").Link;
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 var Main = React.createClass({
   render: function () {
     return (
       <div className="container">
-        <div className="row">
-          <nav className="navbar navbar-default" role="navigation">
-            <div className="navbar-header">
-              <Link className="navbar-brand" to="/">Quotes</Link>
-            </div>
-            <div className="collapse navbar-collapse navbar-ex1-collapse">
-              <ul className="nav navbar-nav navbar-right">
-                <li><Link to="/save-quote"><span className="glyphicon glyphicon-home"></span> Home</Link></li>
-                <li><Link to="/saved-quotes"><span className="glyphicon glyphicon-heart"></span> Favorites</Link></li>
-              </ul>
-            </div>
-          </nav>
-        </div>
+        <Navbar/>
         {this.props.children}
-        <div className="row text-center footer">
-          <p>Copyright &copy; 2017 Radhika Sivarajan</p>
-        </div>
+        <Footer/>
       </div>
     );
   }
